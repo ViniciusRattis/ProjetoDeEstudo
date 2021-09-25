1493d2dfc0c8904f2f385019e4d8fc4b92812802
vinicius Rattis <viniciusrattis@gmail.com>
2021-09-25 17:31:52 -0300

    Implementado a opção fazer consulta do saldo

--- ContaDeBanco/src/main/java/CriandoConta/ConsultandoSaldo.java
+++ ContaDeBanco/src/main/java/CriandoConta/ConsultandoSaldo.java
@@ -1,13 +1,45 @@
 package CriandoConta;
 
+import java.util.Scanner;
+
 public class ConsultandoSaldo {
 
     private String consultaNumeroConta;
+    private String consultaSenha;
 
-    public void consultarSaldo(){
+    public void consultarSaldo(AcessoConta[] contas){
 
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Informe um numero de uma Conta: ");
+        AcessoConta contaEncontrada = encontrarConta(contas, scanner.next());
+
+        if (contaEncontrada == null ){
+            System.out.println("numero conta não encontrada");
+            return;
+        }
+
+        System.out.println("Informe os 6 Digitos da Senha: ");
+        boolean opcao2 = validarSenha(scanner.next(), contaEncontrada);
+
+        if (opcao2) {
+            System.out.println("o Saldo em Conta: " + contaEncontrada.getSaldo());
+        } else{
+            System.out.println("senha invalida.");
+        }
     }
 
+    private boolean validarSenha(String consultaSenha, AcessoConta contaEncontrada) {
+        return consultaSenha.equals(contaEncontrada.getSenha());
+    }
+
+    private AcessoConta encontrarConta(AcessoConta[] contas,String consultaNumeroConta) {
+        for (AcessoConta conta : contas) {
+            if (consultaNumeroConta.equals(conta.getConta())) {
+                return conta;
+            }
+        }
+        return null;
+    }
 }
--- ContaDeBanco/src/main/java/CriandoConta/mainApp.java
+++ ContaDeBanco/src/main/java/CriandoConta/mainApp.java
@@ -4,17 +4,17 @@ public class mainApp {
 
     public static void main(String[] args) {
 
-        AcessoConta[] conta = new AcessoConta[2];
+        AcessoConta[] contas = new AcessoConta[2];
         GerandoNumeroAleatorio geradorNumero = new GerandoNumeroAleatorio();
 
-        conta[0] = new AcessoConta("123456",
+        contas[0] = new AcessoConta("123456",
                 geradorNumero.gerador4Digitos(),
                 geradorNumero.gerador6Digitos(),
                 "Vinicius",
                 "111.111.111-11"
         );
 
-        conta[1] = new AcessoConta("789101",
+        contas[1] = new AcessoConta("789101",
                 geradorNumero.gerador4Digitos(),
                 geradorNumero.gerador6Digitos(),
                 "Tulio",
@@ -25,16 +25,14 @@ public class mainApp {
         // cpf
         // senha
         // agencia = Random
-        // conta = Random
+        // contas = Random
         // saldo = 0
 
-        System.out.println(conta[0]);
+        System.out.println(contas[0]);
         System.out.println();
-        System.out.println(conta[0]);
+        System.out.println(contas[1]);
 
-
-
-        System.out.println(conta[0].getSaldo());
+        new ConsultandoSaldo().consultarSaldo(contas);
 
     }
 }
